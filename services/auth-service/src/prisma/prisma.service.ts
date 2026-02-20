6ecce6cfa54ef1cd673239bfd7fba29ba5273c38
Diego Ivan Solis Cuevas <disc3110@gmail.com>
2026-02-19 21:38:15 -0800

    feat(auth): integrate prisma service with nest

---
+++ services/auth-service/src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    // Pass the adapter to PrismaClient so it uses the DATABASE_URL from env
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
